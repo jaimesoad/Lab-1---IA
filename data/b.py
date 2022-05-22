86ba3716776c931d0ed86259f4fcc57258804b93
jaimesoad <jaimesoad@github.com>
2022-05-22 17:21:52 -0500

    minor changes

--- data/b.py
+++ data/b.py
@@ -1,10 +1,11 @@
 import csv
 
 inputFile  = csv.reader(open("palabras.csv"))
-outputFile = open("dict-es.js", 'w')
+outputFile = open("dict-es5.js", 'w')
 
 outputFile.write("data = [")
 for word in inputFile:
-    outputFile.write(f"\t\"{word[0]}\",\n")
+    if len(word[0]) == 5 and word[0][0] == "s":
+        outputFile.write(f"\t\"{word[0]}\",\n")
 
 outputFile.write("]")
